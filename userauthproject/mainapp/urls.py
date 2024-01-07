5a0a5b35518ca7dea087d3652f2bee3d59f48c0a
Rhythm1821 <alamrawat1821@gmail.com>
2024-01-07 13:36:22 +0530

    creating and deleting post done

--- userauthproject/mainapp/urls.py
+++ userauthproject/mainapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("home",views.home,name="home"),
     path("sign-up",views.sign_up,name="sign_up"),
     path("logout_user",views.logout_user,name="logouts"),
+    path("create-post",views.create_post,name="create_post")
 ]
--- userauthproject/mainapp/views.py
+++ userauthproject/mainapp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from django.contrib.auth import login,logout,authenticate  
-from .forms import RegisterForm
+from django.contrib.auth import login,logout
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm,PostForm
+from .models import Post
 
 # Create your views here.
+@login_required(login_url="/login")
 def home(request):
-    return render(request,"main/home.html")
+    posts=Post.objects.all()
+
+    if request.method=="POST":
+        post_id=request.POST.get("post-id")
+        post = Post.objects.filter(id=post_id).first()
+        if post and post.author==request.user:
+            post.delete()
+    return render(request,"main/home.html",{"posts":posts})
 
 def sign_up(request):
     if request.method=="POST":
@@ -18,6 +27,21 @@ def sign_up(request):
         form = RegisterForm()
 
     return render(request,"registration/sign_up.html",{"form":form})
+
+@login_required(login_url="/login")
+def create_post(request):
+    if request.method=="POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user  # this is how you access the user, currently signed in
+            post.save()
+            return redirect("/home")
+    else:
+        form = PostForm()
+
+    return render(request,'main/create_post.html',{"form":form})
+
 
 # Rhythm422@
 def logout_user(request):
